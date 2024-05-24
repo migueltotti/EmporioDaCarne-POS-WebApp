c6592d73673dfee9453fa533a02744719a868238
Miguel Totti <migueltotti2005@gmail.com>
2024-05-24 12:14:42 -0300

    Create Project

---
+++ EmporioDaCarne-POS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmporioDaCarne_POS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
